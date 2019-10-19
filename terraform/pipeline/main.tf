ea842a2c33a7e8d40e5d41ee7e5f579f86c4f11a
Naveen Padala <naveenpadala@Naveens-mac.local>
2019-10-19 17:47:37 -0400

    more files

---
+++ terraform/pipeline/main.tf
@@ -0,0 +1,14 @@
+//provider "aws" {
+//  region = "${var.region}"
+//  shared_credentials_file = "/Users/naveenpadala/.aws/credentials"
+//  profile = "default"
+//}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c64dd618a49aeee8"
+  instance_type = "${var.instance_type}"
+
+  tags = {
+    Name = "tfTest"
+  }
+}
---
+++ terraform/pipeline/variables.tfvars
@@ -0,0 +1,7 @@
+region = "us-east-2"
+
+aws_profile = "default"
+
+shared_credentials_file = "/Users/naveenpadala/.aws/credentials"
+
+availability_zones = ["us-east-2a", "us-east-2b"]
